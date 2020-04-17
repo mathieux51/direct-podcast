fd25b47229d4a9f9d2507a9a56aaf1ff798cfc45
Mathieu <mathieux51@gmail.com>
2020-04-17 10:55:52 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div``
+const Button = styled.button``
 
 function App() {
+  const audioRef = React.createRef(null)
+  const [isRecording, setIsRecording] = React.useState(false)
+  const [mediaRec, setMediaRec] = React.useState(null)
+
+  if (mediaRec) {
+    let samples = []
+    mediaRec.ondataavailable = (evt) => {
+      samples.push(evt.data)
+      if (mediaRec.state === "inactive") {
+        const blob = new Blob(samples, { type: "audio/mpeg-3" })
+        const u = URL.createObjectURL(blob)
+        audioRef.current.src = u
+        audioRef.current.controls = true
+        audioRef.current.autoplay = true
+        setMediaRec(null)
+        const a = document.createElement("a")
+        a.href = u
+        a.download = "test.mpg3"
+        a.click()
+        window.URL.revokeObjectURL(u)
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        if (!mediaRec) {
+          setMediaRec(new MediaRecorder(stream))
+        }
+      })
+      .catch((error) => console.error(error))
+  }, [mediaRec])
+
+  const handleClick = (evt) => {
+    if (!isRecording) {
+      setIsRecording(true)
+      mediaRec.start()
+      return
+    }
+
+    setIsRecording(false)
+    mediaRec.stop()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1>Direct podcast</h1>
+      <Button onClick={handleClick}>Record</Button>
+      <audio ref={audioRef} />
+    </Container>
+  )
 }
 
-export default App;
+export default App
