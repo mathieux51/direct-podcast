9b9c62c4891f6ef79943ad26d7fdcdb1e1f974de
Mathieu <mathieux51@gmail.com>
2020-04-19 22:45:13 +0200

    Add polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,27 +1,28 @@
 // source: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 // Older browsers might not implement mediaDevices at all, so we set an empty object first
 if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {};
+  navigator.mediaDevices = {}
 }
 
 // Some browsers partially implement mediaDevices. We can't just assign an object
 // with getUserMedia as it would overwrite existing properties.
 // Here, we will just add the getUserMedia property if it's missing.
 if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = function(constraints) {
-
+  navigator.mediaDevices.getUserMedia = function (constraints) {
     // First get ahold of the legacy getUserMedia, if present
-    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
     // Some browsers just don't implement it - return a rejected promise with an error
     // to keep a consistent interface
     if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      return Promise.reject(
+        new Error("getUserMedia is not implemented in this browser")
+      )
     }
 
     // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-    return new Promise(function(resolve, reject) {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
+    return new Promise(function (resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject)
+    })
   }
 }
