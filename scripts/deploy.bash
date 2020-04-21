1159d6902034aa549a1eaf6e8e34896b60bffae6
Mathieu <mathieux51@gmail.com>
2020-04-21 07:27:12 +0200

    Update deploy script

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -3,23 +3,5 @@
 set -o errexit
 set -o nounset
 
-PROJECT_NAME='directpodcast'
-
-# build
-npx now \
-  --no-clipboard
-
-# list all projects in temp file
-npx now ls ${PROJECT_NAME} > temp
-
-# get the alias of last deployment
-ALIAS=$(cat temp | grep ${PROJECT_NAME} | awk '{ print $2 }' | head -1)
-
-# alias last deployement
-npx now alias $ALIAS "www.${PROJECT_NAME}.fr"
-npx now alias $ALIAS "${PROJECT_NAME}.fr"
-
-# clean up
-unset ALIAS
-rm temp
+ now --prod && now alias direct-podcast.now.sh directpodcast.fr && now alias direct-podcast.now.sh www.directpodcast.fr
 
