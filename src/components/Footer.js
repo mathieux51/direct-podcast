d364fd6dc583dd35d288b63d374b30aa0f460660
Mathieu <mathieux51@gmail.com>
2020-04-21 07:52:02 +0200

    Update footnote font-size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,7 +45,7 @@ const BottomNote = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 10px;
+  font-size: 14px;
 `
 
 const Footer = ({ className }) => {
