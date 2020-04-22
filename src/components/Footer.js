9c2f80c31a9968f8aa990215b4849ab26692052d
Mathieu <mathieux51@gmail.com>
2020-04-22 11:35:24 +0200

    Update font-size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,7 +45,7 @@ const BottomNote = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 14px;
+  font-size: 15px;
 `
 
 const Footer = ({ className }) => {
