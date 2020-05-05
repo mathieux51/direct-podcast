a1ba027d6f2a3996010a63d2db8608a00613d4ed
Mathieu <mathieux51@gmail.com>
2020-05-05 21:34:21 +0200

    Add deploy to github actions

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -3,5 +3,12 @@
 set -o errexit
 set -o nounset
 
- now --prod && now alias direct-podcast.now.sh directpodcast.fr
+npx now \
+  --no-clipboard \
+  -t ${NOW_TOKEN} \
+  -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF} \
+  -b RECAPTCHA_CLIENT_SIDE=${RECAPTCHA_CLIENT_SIDE} \
+  --prod
+
+now alias direct-podcast.now.sh directpodcast.fr
 
