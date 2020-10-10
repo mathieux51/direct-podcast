88ee3889ee5e627eea7c3752489fd842491565a0
Mathieu <mathieux51@gmail.com>
2020-10-10 12:50:50 +0200

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ npx now \
   -t ${NOW_TOKEN} \
   -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF} \
   -b REACT_APP_RECAPTCHA_CLIENT_SIDE=${REACT_APP_RECAPTCHA_CLIENT_SIDE} \
-  -b SENTRY_AUTH_TOKEN=${SENTRY_AUTH_TOKEN}
+  -b SENTRY_AUTH_TOKEN=${SENTRY_AUTH_TOKEN} \
   --prod
 
 npx now \
