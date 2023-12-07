e16d460a201240284831f9dd10f0b9b2bacb4cc1
Mathieu <mathieux51@gmail.com>
2023-12-07 10:36:34 +0100

    rename src pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
-const withPreact = require('next-plugin-preact')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withPreact = require('next-plugin-preact')
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-module.exports = withPreact(
-  withBundleAnalyzer({
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      RECAPTCHA_CLIENT_SIDE: process.env.RECAPTCHA_CLIENT_SIDE,
-    },
-  })
-)
+// module.exports = withPreact(
+  // withBundleAnalyzer({
+  //   publicRuntimeConfig: {
+  //     // Will be available on both server and client
+  //     RECAPTCHA_CLIENT_SIDE: process.env.RECAPTCHA_CLIENT_SIDE,
+  //   },
+  // })
+// )
+module.exports = {}
